36e7d57bcea9c71d195e6368b2daf64b14e4f6a6
lebre <lebre.soft.info@gmail.com>
2020-09-08 08:56:31 -0300

    adicionando arquivo bin

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -13,6 +14,5 @@ $buscador = new Buscador($client,$crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso){
-
-    echo exibeMensagem($curso);
+    exibeMensagem($curso);
 }
